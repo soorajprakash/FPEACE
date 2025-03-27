8e199b3db36f4c1bcc71abbf7376ac0f9a7e7ea8
Sooraj Prakash <soorajprakash45@gmail.com>
2025-03-27 17:43:15 +0530

    - Weapon look location and rotation lag

--- Source/FPEACE/Private/FPCBlueprintFunctionLibrary.cpp
+++ Source/FPEACE/Private/FPCBlueprintFunctionLibrary.cpp
@@ -2,6 +2,7 @@
 
 
 #include "FPCBlueprintFunctionLibrary.h"
+#include "AnimNodes/AnimNode_SequenceEvaluator.h"
 
 bool UFPCBlueprintFunctionLibrary::GetIfAnimSequenceHasRootMotionDelta(UAnimSequence* TargetSequence)
 {
@@ -15,6 +16,23 @@ bool UFPCBlueprintFunctionLibrary::GetIfAnimSequenceHasLoopingEnabled(UAnimSeque
 	if (!TargetSequence)
 		return false;
 	return TargetSequence->bLoop;
+}
+
+bool UFPCBlueprintFunctionLibrary::GetIfEvaluatorHasValidSequence(const FAnimNode_SequenceEvaluator& SequenceEvaluatorNode)
+{
+	return SequenceEvaluatorNode.GetSequence() != nullptr;
+}
+
+bool UFPCBlueprintFunctionLibrary::GetIfEvaluatorSequenceIsValidOrEqual(const FAnimNode_SequenceEvaluator& SequenceEvaluatorNode, const UAnimSequenceBase* Sequence)
+{
+	UAnimSequenceBase* EvaluatorSequence = SequenceEvaluatorNode.GetSequence();
+
+	if (EvaluatorSequence == nullptr)
+		return false;
+	if (EvaluatorSequence == Sequence)
+		return true;
+	
+	return false;
 }
 
 EAdditiveAnimationType UFPCBlueprintFunctionLibrary::GetAnimSequenceAdditiveType(UAnimSequence* TargetSequence)
--- Source/FPEACE/Private/FPCBlueprintFunctionLibrary.h
+++ Source/FPEACE/Private/FPCBlueprintFunctionLibrary.h
@@ -6,6 +6,7 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "FPCBlueprintFunctionLibrary.generated.h"
 
+struct FAnimNode_SequenceEvaluator;
 /**
  * 
  */
@@ -15,12 +16,18 @@ class UFPCBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe))
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe), Category="FPEACE|Animation")
 	static bool GetIfAnimSequenceHasRootMotionDelta(UAnimSequence* TargetSequence);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe))
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe), Category="FPEACE|Animation")
 	static bool GetIfAnimSequenceHasLoopingEnabled(UAnimSequence* TargetSequence);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe))
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe), Category="FPEACE|Animation")
+	static bool GetIfEvaluatorHasValidSequence(const FAnimNode_SequenceEvaluator& SequenceEvaluatorNode);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe), Category="FPEACE|Animation")
+	static bool GetIfEvaluatorSequenceIsValidOrEqual(const FAnimNode_SequenceEvaluator& SequenceEvaluatorNode, const UAnimSequenceBase* Sequence);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta=(BlueprintThreadSafe), Category="FPEACE|Animation")
 	static EAdditiveAnimationType GetAnimSequenceAdditiveType(UAnimSequence* TargetSequence);
 };
