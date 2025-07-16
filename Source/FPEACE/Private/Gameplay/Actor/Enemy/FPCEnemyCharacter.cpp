6de6ca6bdb98f21b8248c00b3c48a9d76e7e986a
Sooraj Prakash <soorajprakash45@gmail.com>
2025-07-16 09:59:55 +0530

    - Temp Commit

---
+++ Source/FPEACE/Private/Gameplay/Actor/Enemy/FPCEnemyCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright © 2025 Sooraj Prakash. All rights reserved.Unauthorized distribution or sharing of this code is prohibited.
+
+
+#include "FPCEnemyCharacter.h"
+
+
+// Sets default values
+AFPCEnemyCharacter::AFPCEnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFPCEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPCEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AFPCEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/FPEACE/Private/Gameplay/Actor/Enemy/FPCEnemyCharacter.h
@@ -0,0 +1,28 @@
+﻿// Copyright © 2025 Sooraj Prakash. All rights reserved.Unauthorized distribution or sharing of this code is prohibited.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gameplay/Actor/FPCCharacter.h"
+#include "FPCEnemyCharacter.generated.h"
+
+UCLASS()
+class FPEACE_API AFPCEnemyCharacter : public AFPCCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPCEnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
